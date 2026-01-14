1e4f0375577bbe0b14b20235b78dc45bc9d52854
Hillary Koros <koroshillary12@gmail.com>
2026-01-14 10:36:43 +0300

    EAFW FloodWatch - Initial consolidation
    Components:
    - geomanager-web: CMS backend (Django/Wagtail)
    - geomapviewer: Map viewer frontend (React)
    - geomanager: Core geospatial library
    - eafw-mapserver: MapServer & MapCache
    - floodwatch-jobs: Background job scheduler
    
    Features:
    - CMS_BASE_URL for environment-agnostic deployment
    - Docker compose staging configuration
    - Multi-model ensemble flood forecasting

--- forecastmanager/forecastmanager/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Forecast
-
-
-class ForecastAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Forecast, ForecastAdmin)
--- forecastmanager/forecastmanager/static/forecastmanager/js/data-parameter-widget-controller.js
+++
@@ -1,7 +0,0 @@
-class DataParameterWidgetController extends window.StimulusModule.Controller {
-    connect() {
-        new DataParameterWidget(this.element.id, this.element.value);
-    }
-}
-
-window.wagtail.app.register('data-parameter-widget', DataParameterWidgetController);
