b8af5e9f5392b29330033de00a3879e62ee7efc5
Hillary Koros <koroshillary12@gmail.com>
2026-04-09 08:41:33 +0300

    fix: remove min zoom restriction on mapviewer (was locked at zoom 4)

--- eafw_geomapviewer/src/utils/constants.js
+++ eafw_geomapviewer/src/utils/constants.js
@@ -5,4 +5,4 @@ export const CMS_API = process.env.CMS_API || "https://eahazardswatch.icpac.net/
 export const DEFAULT_CENTER_LAT = parseFloat(process.env.DEFAULT_CENTER_LAT || 5.73)
 export const DEFAULT_CENTER_LNG = parseFloat(process.env.DEFAULT_CENTER_LNG || 36.84)
 export const DEFAULT_ZOOM_LEVEL = parseFloat(process.env.DEFAULT_ZOOM_LEVEL || 4)
-export const DEFAULT_MIN_ZOOM = parseFloat(process.env.DEFAULT_MIN_ZOOM || 4)
+export const DEFAULT_MIN_ZOOM = parseFloat(process.env.DEFAULT_MIN_ZOOM || 0)
